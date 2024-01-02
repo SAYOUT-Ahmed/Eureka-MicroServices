ac3cb89148e412848f6a19d1fb98feacb8dcb097
Ahmed <ahmed.sayout18@gmail.com>
2024-01-02 02:07:18 +0100

    controller update

--- eureka-ms/car/src/main/java/com/example/car/controllers/CarController.java
+++ eureka-ms/car/src/main/java/com/example/car/controllers/CarController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+//Car controller class
 @RestController
 @RequestMapping("api/car")
 public class CarController {
